2d95394146dea46428cd721332ca668391db385e
Bruno Capuano <bcapuano@gmail.com>
2025-03-31 13:50:34 -0400

    Add LabsPhi4-Chat-04-ChatMode and update configurations
    DETAILS
    
    - Updated `settings.json` to ignore "main" branch in PRs.
    - Added `LabsPhi4-Chat-04-ChatMode` project to `readme.md`.
    - Included new project in `LabsPhi.sln` with Debug/Release configs.
    - Modified `LabsPhi4-MultiModal-01Images.csproj` for CUDA/DirectML support and updated package references.
    - Enhanced `Program.cs` for GPU model initialization and streamlined setup.
    - Created `LabsPhi4-Chat-04-ChatMode.csproj` with necessary properties and package references.
    - Implemented token streaming generator in `Program.cs` for interactive chat functionality.

---
+++ md/04.HOL/dotnet/src/LabsPhi4-Chat-04-ChatMode/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.ML.OnnxRuntimeGenAI;
+
+// Phi4
+var modelPath = @"d:\phi\models\Phi-4-mini-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32-acc-level-4\";
+
+
+// initialize model
+var model = new Model(modelPath);
+var tokenizer = new Tokenizer(model);
+var generatorParams = new GeneratorParams(model);
+
+generatorParams.SetSearchOption("max_length", 2048);
+generatorParams.SetSearchOption("past_present_share_buffer", false);
+using var tokenizerStream = tokenizer.CreateStream();
+
+using var generator = new Generator(model, generatorParams);
+Console.Write("Prompt: ");
+var prompt = Console.ReadLine();
+
+// Example Phi-3 template
+var sequences = tokenizer.Encode($"<|user|>{prompt}<|end|><|assistant|>");
+
+do
+{
+    generator.AppendTokenSequences(sequences);
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    while (!generator.IsDone())
+    {
+        generator.GenerateNextToken();
+        Console.Write(tokenizerStream.Decode(generator.GetSequence(0)[^1]));
+    }
+    Console.WriteLine();
+    watch.Stop();
+    var runTimeInSeconds = watch.Elapsed.TotalSeconds;
+    var outputSequence = generator.GetSequence(0);
+    var totalTokens = outputSequence.Length;
+    Console.WriteLine($"Streaming Tokens: {totalTokens} Time: {runTimeInSeconds:0.00} Tokens per second: {totalTokens / runTimeInSeconds:0.00}");
+    Console.Write("Next prompt: ");
+    prompt = Console.ReadLine();
+    sequences = tokenizer.Encode($"<|user|>{prompt}<|end|><|assistant|>");
+} while (!string.IsNullOrEmpty(prompt));
+
